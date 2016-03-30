47fd47ea4adb20ef5273ed80846449c60d95d60b
Yang Cao <yacao@microsoft.com>
2016-03-30 16:32:14 -0400

    Create a storage provider for consumer to abstract out all the store implementations

---
+++ storages/src/main/java/com/microsoft/alm/storage/StorageProvider.java
@@ -0,0 +1,196 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
+package com.microsoft.alm.storage;
+
+import com.microsoft.alm.helpers.Debug;
+import com.microsoft.alm.helpers.SystemHelper;
+import com.microsoft.alm.secret.Credential;
+import com.microsoft.alm.secret.Secret;
+import com.microsoft.alm.secret.Token;
+import com.microsoft.alm.secret.TokenPair;
+import com.microsoft.alm.storage.macosx.KeychainSecurityBackedCredentialStore;
+import com.microsoft.alm.storage.macosx.KeychainSecurityBackedTokenStore;
+import com.microsoft.alm.storage.posix.GnomeKeyringBackedCredentialStore;
+import com.microsoft.alm.storage.posix.GnomeKeyringBackedTokenPairStore;
+import com.microsoft.alm.storage.posix.GnomeKeyringBackedTokenStore;
+import com.microsoft.alm.storage.posix.internal.GnomeKeyringBackedSecureStore;
+import com.microsoft.alm.storage.windows.CredManagerBackedCredentialStore;
+import com.microsoft.alm.storage.windows.CredManagerBackedTokenStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StorageProvider {
+
+    private static Logger logger  = LoggerFactory.getLogger(StorageProvider.class);
+
+    public enum SecureOption {
+        /**
+         * The store must be secure, i.e. generally the storage needs to be password protected
+         * and data potentially is encrypted
+         *
+         * However, this program makes no assertion on *how* secure the storage really is.  It's only
+         * an attribute on the storage
+         */
+        MUST,
+
+        /**
+         * Prefer a secure storage, but if none is available, a unprotected, non secure storage will be returned
+         */
+        PREFER
+    }
+
+    private static final List<SecretStore<Token>> PERSISTED_TOKEN_STORE_CANDIDATES;
+
+    private static final List<SecretStore<TokenPair>> PERSISTED_TOKENPAIR_STORE_CANDIDATES;
+
+    private static final List<SecretStore<Credential>> PERSISTED_CREDENTIAL_STORE_CANDIDATES;
+
+    static {
+        List<SecretStore<Token>> tokenStoreCandidates = new ArrayList<SecretStore<Token>>();
+        List<SecretStore<TokenPair>> tokenPairStoreCandidates = new ArrayList<SecretStore<TokenPair>>();
+        List<SecretStore<Credential>> credentialStoreCandidates = new ArrayList<SecretStore<Credential>>();
+
+        if (SystemHelper.isWindows()) {
+            tokenStoreCandidates.add(new CredManagerBackedTokenStore());
+            credentialStoreCandidates.add(new CredManagerBackedCredentialStore());
+        }
+
+        if (SystemHelper.isMac()) {
+            tokenStoreCandidates.add(new KeychainSecurityBackedTokenStore());
+            credentialStoreCandidates.add(new KeychainSecurityBackedCredentialStore());
+        }
+
+        if (SystemHelper.isLinux() && GnomeKeyringBackedSecureStore.isGnomeKeyringSupported()) {
+            tokenStoreCandidates.add(new GnomeKeyringBackedTokenStore());
+            credentialStoreCandidates.add(new GnomeKeyringBackedCredentialStore());
+            tokenPairStoreCandidates.add(new GnomeKeyringBackedTokenPairStore());
+        }
+
+        tokenStoreCandidates.add(new InsecureFileBackedTokenStore());
+        credentialStoreCandidates.add(new InsecureFileBackedCredentialStore());
+
+        PERSISTED_TOKEN_STORE_CANDIDATES = tokenStoreCandidates;
+        PERSISTED_TOKENPAIR_STORE_CANDIDATES = tokenPairStoreCandidates;
+        PERSISTED_CREDENTIAL_STORE_CANDIDATES = credentialStoreCandidates;
+    }
+
+    public static SecretStore<Token> getTokenStorage(final boolean persist, final SecureOption secureOption) {
+        Debug.Assert(secureOption != null, "secureOption cannot be null");
+
+        logger.info("Getting a {} token store that {} be secure", persist ? "persistent" : "non-persistent",
+                secureOption == SecureOption.MUST ? "must" : "could");
+
+        final NonPersistentStoreGenerator<Token> inmemoryStoreGenerator = new NonPersistentStoreGenerator<Token>() {
+            @Override
+            public SecretStore<Token> getPotentialInsecureNonPersistentStore() {
+                return new InsecureInMemoryStore<Token>();
+            }
+
+            @Override
+            public SecretStore<Token> getSecureNonPersistentStore() {
+                logger.warn("Do not have any secure non-persistent stores available.");
+                return null;
+            }
+        };
+
+        return getStore(persist, secureOption, PERSISTED_TOKEN_STORE_CANDIDATES, inmemoryStoreGenerator);
+    }
+
+    public static SecretStore<TokenPair> getTokenPairStorage(final boolean persist, final SecureOption secureOption) {
+        Debug.Assert(secureOption != null, "secureOption cannot be null");
+
+        logger.info("Getting a {} tokenPair store that {} be secure", persist ? "persistent" : "non-persistent",
+                secureOption == SecureOption.MUST ? "must" : "could");
+
+        final NonPersistentStoreGenerator<TokenPair> inmemoryStoreGenerator = new NonPersistentStoreGenerator<TokenPair>() {
+            @Override
+            public SecretStore<TokenPair> getPotentialInsecureNonPersistentStore() {
+                return new InsecureInMemoryStore<TokenPair>();
+            }
+
+            @Override
+            public SecretStore<TokenPair> getSecureNonPersistentStore() {
+                logger.warn("Do not have any secure non-persistent stores available.");
+                return null;
+            }
+        };
+
+        return getStore(persist, secureOption, PERSISTED_TOKENPAIR_STORE_CANDIDATES, inmemoryStoreGenerator);
+    }
+
+    public static SecretStore<Credential> getCredentialStorage(final boolean persist, final SecureOption secureOption) {
+        Debug.Assert(secureOption != null, "secureOption cannot be null");
+
+        logger.info("Getting a {} credential store that {} be secure", persist ? "persistent" : "non-persistent",
+                secureOption == SecureOption.MUST ? "must" : "could");
+
+        final NonPersistentStoreGenerator<Credential> inmemoryStoreGenerator = new NonPersistentStoreGenerator<Credential>() {
+            @Override
+            public SecretStore<Credential> getPotentialInsecureNonPersistentStore() {
+                return new InsecureInMemoryStore<Credential>();
+            }
+
+            @Override
+            public SecretStore<Credential> getSecureNonPersistentStore() {
+                logger.warn("Do not have any secure non-persistent stores available.");
+                return null;
+            }
+        };
+
+        return getStore(persist, secureOption, PERSISTED_CREDENTIAL_STORE_CANDIDATES, inmemoryStoreGenerator);
+    }
+
+    private static <E extends Secret> SecretStore<E> findSecureStore(final List<SecretStore<E>> stores) {
+        for (final SecretStore<E> store : stores) {
+            if (store.isSecure()) {
+                return store;
+            }
+        }
+
+        return null;
+    }
+
+    private static <E extends Secret> SecretStore<E> findPersistedStore(final SecureOption secureOption,
+                                                                 final List<SecretStore<E>> stores) {
+        SecretStore<E> candidate = findSecureStore(stores);
+
+        if (candidate == null && secureOption == SecureOption.PREFER) {
+            // just return any store from the list since none of them is secure
+            if (!stores.isEmpty()) {
+                candidate = stores.get(0);
+            }
+        }
+
+        // nullable
+        return candidate;
+    }
+
+    static <E extends Secret> SecretStore<E> getStore(final boolean persist, final SecureOption secureOption,
+            final List<SecretStore<E>> stores, final NonPersistentStoreGenerator<E> nonPersistentStoreGenerator) {
+        Debug.Assert(nonPersistentStoreGenerator != null, "nonPersistentStoreGenerator cannot be null.");
+        Debug.Assert(stores != null, "stores cannot be null.");
+
+        SecretStore<E> candidate = null;
+        if (persist) {
+            candidate = findPersistedStore(secureOption, stores);
+        } else {
+            // not persisted
+            if (secureOption == SecureOption.PREFER) {
+                candidate = nonPersistentStoreGenerator.getPotentialInsecureNonPersistentStore();
+            } else {
+                candidate = nonPersistentStoreGenerator.getSecureNonPersistentStore();
+            }
+        }
+
+        return candidate;
+    }
+
+    interface NonPersistentStoreGenerator<E extends Secret> {
+        SecretStore<E> getPotentialInsecureNonPersistentStore();
+        SecretStore<E> getSecureNonPersistentStore();
+    }
+}
---
+++ storages/src/test/java/com/microsoft/alm/storage/StorageProviderTest.java
@@ -0,0 +1,88 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
+package com.microsoft.alm.storage;
+
+import com.microsoft.alm.secret.Token;
+import com.microsoft.alm.storage.StorageProvider.NonPersistentStoreGenerator;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.microsoft.alm.storage.StorageProvider.SecureOption;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class StorageProviderTest {
+
+    private NonPersistentStoreGenerator<Token> generator = new NonPersistentStoreGenerator<Token>() {
+        @Override
+        public SecretStore<Token> getPotentialInsecureNonPersistentStore() {
+            return getStore(false);
+        }
+
+        @Override
+        public SecretStore<Token> getSecureNonPersistentStore() {
+            return null;
+        }
+    };
+
+    @Test
+    public void withAvailableSecureStore_shouldReturnSecureStore() throws Exception {
+        List<SecretStore<Token>> candidates = new ArrayList<SecretStore<Token>>();
+        //Add one persisted secure store to it
+        candidates.add(getStore(true));
+
+        final SecretStore<Token> actual = StorageProvider.getStore(true, SecureOption.MUST, candidates, generator);
+        assertTrue(actual.isSecure());
+    }
+
+    @Test
+    public void noAvailableSecureStore_shouldReturnNull() throws Exception {
+        List<SecretStore<Token>> candidates = new ArrayList<SecretStore<Token>>();
+
+        final SecretStore<Token> actual = StorageProvider.getStore(true, SecureOption.MUST, candidates, generator);
+        assertNull(actual);
+    }
+
+    @Test
+    public void nonPersisted_MustBeSecure_shouldUseGenerator() throws Exception {
+        List<SecretStore<Token>> candidates = new ArrayList<SecretStore<Token>>();
+        //Add one persisted secure store to it
+        candidates.add(getStore(true));
+
+        final SecretStore<Token> actual = StorageProvider.getStore(false, SecureOption.MUST, candidates, generator);
+        assertNull(actual);
+    }
+
+    @Test
+    public void nonPersisted_doNotCareSecurity_shouldUseGenerator() throws Exception {
+        List<SecretStore<Token>> candidates = new ArrayList<SecretStore<Token>>();
+        //Add one persisted secure store to it
+        candidates.add(getStore(true));
+
+        final SecretStore<Token> actual = StorageProvider.getStore(false, SecureOption.PREFER, candidates, generator);
+        assertFalse(actual.isSecure());
+    }
+
+    private SecretStore<Token> getStore(final boolean secure) {
+        return new SecretStore<Token>() {
+            @Override
+            public Token get(String key) { return null; }
+
+            @Override
+            public boolean delete(String key) { return false; }
+
+            @Override
+            public boolean add(String key, Token secret) { return false; }
+
+            @Override
+            public boolean isSecure() {
+                return secure;
+            }
+        };
+    }
+
+}
