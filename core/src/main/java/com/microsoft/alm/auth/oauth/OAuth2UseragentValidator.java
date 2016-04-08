f2997b8fc06681db4ac55c8d876defdecde57e23
Yang Cao <yacao@microsoft.com>
2016-04-08 15:02:31 -0400

    Consume oauth2-useragent's scanner ability and make the class testable

--- core/src/main/java/com/microsoft/alm/auth/oauth/OAuth2UseragentValidator.java
+++ core/src/main/java/com/microsoft/alm/auth/oauth/OAuth2UseragentValidator.java
@@ -4,42 +4,27 @@
 package com.microsoft.alm.auth.oauth;
 
 import com.microsoft.alm.oauth2.useragent.Provider;
-
-import java.util.Collections;
-import java.util.List;
+import com.microsoft.alm.oauth2.useragent.ProviderScanner;
+import com.microsoft.alm.oauth2.useragent.UserAgentImpl;
 
 /**
  * This class verifies the availability of OAuth2-useragent on the current platform
- *
- * This is a interim class that will go away once we merge
- * https://github.com/Microsoft/oauth2-useragent/pull/21
  */
 public class OAuth2UseragentValidator {
 
-    // This is a hack since it depends on oauth2-useragent implementations
-    // This should be removed as soon as the pull request mentioned is merged
-    private static final Provider provider = new Provider("OAuth2UseragentValidator") {
-        @Override
-        public List<String> checkRequirements() {
-            return Collections.<String>emptyList();
-        }
-
-        @Override
-        public void augmentProcessParameters(List<String> list, List<String> list1) { }
-    };
+    private final ProviderScanner scanner = new UserAgentImpl();
 
     /**
      * Determines if oauth2 useragent can be used on the current running system.
      *
-     * Be conservative, could return false negative.
-     *
      * @return {@code true} if oauth2-useragent can be used 100% positively
      *         {@code false} with any doubts
      */
-    public static boolean oauth2UserAgentAvailable() {
+    public boolean oauth2UserAgentAvailable() {
         // not tests are worthy adding since I don't control this implementation
-        final List<String> javaFxRequirements = provider.JAVA_FX.checkRequirements();
+        final Provider provider = scanner.findCompatibleProvider("JavaFx");
 
-        return javaFxRequirements.isEmpty();
+        // I only want JavaFx provider, don't want to use device profile provider while we still have SWT
+        return provider != null && "JavaFx".equals(provider.getClassName());
     }
 }
