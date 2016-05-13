2b1dd5afb5aaf8df99e3cf687cf91c527842f3d8
Oli Dagenais <olivida@microsoft.com>
2016-05-12 21:30:36 -0400

    Move TokenPair XML serialization out of XmlHelper

--- common/src/main/java/com/microsoft/alm/helpers/XmlHelper.java
+++ common/src/main/java/com/microsoft/alm/helpers/XmlHelper.java
@@ -3,12 +3,9 @@
 
 package com.microsoft.alm.helpers;
 
-import com.microsoft.alm.secret.TokenPair;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
 
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
@@ -60,40 +57,4 @@ public class XmlHelper {
         }
     }
 
-    public static TokenPair fromXmlToTokenPair(final Node tokenPairNode) {
-        TokenPair value;
-
-        String accessToken = null;
-        String refreshToken = null;
-
-        final NodeList propertyNodes = tokenPairNode.getChildNodes();
-        for (int v = 0; v < propertyNodes.getLength(); v++) {
-            final Node propertyNode = propertyNodes.item(v);
-            final String propertyName = propertyNode.getNodeName();
-            if ("accessToken".equals(propertyName)) {
-                accessToken = XmlHelper.getText(propertyNode);
-            } else if ("refreshToken".equals(propertyName)) {
-                refreshToken = XmlHelper.getText(propertyNode);
-            }
-        }
-
-        value = new TokenPair(accessToken, refreshToken);
-        return value;
-    }
-
-    public static Element toXml(final Document document, final TokenPair tokenPair) {
-        final Element valueNode = document.createElement("value");
-
-        final Element accessTokenNode = document.createElement("accessToken");
-        final Text accessTokenValue = document.createTextNode(tokenPair.AccessToken.Value);
-        accessTokenNode.appendChild(accessTokenValue);
-        valueNode.appendChild(accessTokenNode);
-
-        final Element refreshTokenNode = document.createElement("refreshToken");
-        final Text refreshTokenValue = document.createTextNode(tokenPair.RefreshToken.Value);
-        refreshTokenNode.appendChild(refreshTokenValue);
-        valueNode.appendChild(refreshTokenNode);
-
-        return valueNode;
-    }
 }
--- common/src/test/java/com/microsoft/alm/helpers/XmlHelperTest.java
+++ common/src/test/java/com/microsoft/alm/helpers/XmlHelperTest.java
@@ -3,17 +3,7 @@
 
 package com.microsoft.alm.helpers;
 
-import com.microsoft.alm.secret.TokenPair;
 import org.junit.Before;
-import org.junit.Test;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.ByteArrayInputStream;
-
-import static org.junit.Assert.assertEquals;
 
 public class XmlHelperTest {
 
@@ -22,36 +12,6 @@ public class XmlHelperTest {
     @Before
     public void setUp() {
         underTest = new XmlHelper();
-    }
-
-    @Test
-    public void xmlTokenPairSerialization_roundTrip() throws Exception {
-        final TokenPair tokenPair =
-            new TokenPair("9297fb18-46d0-4846-97ca-ab8dd3b55729", "d15281b1-03f1-4581-90d3-4527d9cf4147");
-        final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        final DocumentBuilder builder = dbf.newDocumentBuilder();
-        final Document serializationDoc = builder.newDocument();
-
-        final Element element = underTest.toXml(serializationDoc, tokenPair);
-
-        serializationDoc.appendChild(element);
-        final String actualXmlString = XmlHelper.toString(serializationDoc);
-        final String expectedXmlString =
-                "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
-                        "<value>\n" +
-                        "    <accessToken>9297fb18-46d0-4846-97ca-ab8dd3b55729</accessToken>\n" +
-                        "    <refreshToken>d15281b1-03f1-4581-90d3-4527d9cf4147</refreshToken>\n" +
-                        "</value>";
-        StringHelperTest.assertLinesEqual(expectedXmlString, actualXmlString);
-
-        final ByteArrayInputStream bais = new ByteArrayInputStream(actualXmlString.getBytes());
-        final Document deserializationDoc = builder.parse(bais);
-        final Element rootNode = deserializationDoc.getDocumentElement();
-
-        final TokenPair actualTokenPair = underTest.fromXmlToTokenPair(rootNode);
-
-        assertEquals(tokenPair.AccessToken.Value, actualTokenPair.AccessToken.Value);
-        assertEquals(tokenPair.RefreshToken.Value, actualTokenPair.RefreshToken.Value);
     }
 
 }
