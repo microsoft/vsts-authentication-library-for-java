481eaac086ccaf274d5221846f5ed1d5515fc97c
Olivier Dagenais <olivier.dagenais@gmail.com>
2016-06-29 21:28:24 -0400

    Add skeleton IOHelperTest

---
+++ common/src/test/java/com/microsoft/alm/helpers/IOHelperTest.java
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
+package com.microsoft.alm.helpers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * A class to test {@link IOHelper}.
+ */
+public class IOHelperTest {
+
+}
